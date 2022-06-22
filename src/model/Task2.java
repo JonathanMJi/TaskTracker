16eb12c2c6cfee63baf5c9ffa29997877bf09391
Jonathan Ji <99520267+JonathanMJi@users.noreply.github.com>
2022-06-21 21:18:30 -0500

    Add files via upload

--- src/model/Task2.java
+++ src/model/Task2.java
@@ -1,1 +1,49 @@
+package model;
+import java.time.LocalDate;
 
+public class Task {
+	private int id;
+	private String name;
+	private LocalDate duedate;
+	private String taskstatus;
+	
+	
+	public Task(String name, LocalDate duedate, String taskstatus) {
+		super();
+		this.name = name;
+		this.duedate = duedate;
+		this.taskstatus = taskstatus;
+	}
+	public Task(int id, String name, LocalDate duedate, String taskstatus) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.duedate = duedate;
+		this.taskstatus = taskstatus;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void name(String name) {
+		this.name =name;
+	}
+	public LocalDate getDuedate() {
+		return duedate;
+	}
+	public void setDuedate(LocalDate duedate) {
+		this.duedate = duedate;
+	}
+	public String getTaskstatus() {
+		return taskstatus;
+	}
+	public void setTaskstatus(String taskstatus) {
+		this.taskstatus = taskstatus;
+	}
+	
+}
